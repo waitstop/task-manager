9c74db3769948a59caaebe4fe64889dc23351809
Александр Казаков <29281112@s.mfua.ru>
2023-09-19 21:01:35 +0300

    Theme Switcher

--- components/ThemeSwitcher/ThemeSwitcher.tsx
+++ components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,20 +1,22 @@
 "use client"
 
-import MoonIcon from "@/public/icons/moon.svg";
-import SunIcon from "@/public/icons/sun.svg";
+
 import variables from "@/app/variables.module.scss";
 import {ChangeEvent, useEffect, useState} from "react";
+import {motion} from "framer-motion"
+import MoonIcon from "@/public/icons/moon.svg";
+import SunIcon from "@/public/icons/sun.svg";
 
 const ThemeSwitcher = () => {
-    const [theme, setTheme] = useState<"light" | "dark">(()=>{
+    const [theme, setTheme] = useState<"light" | "dark">(() => {
         if (typeof window === "undefined") return "light";
-        if(localStorage.getItem("theme")){
+        if (localStorage.getItem("theme")) {
             return localStorage.getItem("theme") === "dark" ? "dark" : "light"
         }
-        if(!window.matchMedia) return "light";
+        if (!window.matchMedia) return "light";
         return (window.matchMedia("(prefers-color-scheme: dark)").matches) ? "dark" : "light";
     })
-    useEffect(()=>{
+    useEffect(() => {
         if (typeof window === "undefined") return
         localStorage.setItem("theme", theme)
         document.documentElement.setAttribute("data-theme", theme)
@@ -25,10 +27,16 @@ const ThemeSwitcher = () => {
     return (
         <div>
             <label htmlFor={"theme-switcher"}>
-                {theme === "dark" ?
-                    <SunIcon stroke={`rgb(${variables.text})`}/> :
-                    <MoonIcon stroke={`rgb(${variables.text})`}/>
-                }
+                <motion.div key={theme}
+                            className={"overflow-hidden"}
+                            initial={theme === "dark" ? {rotate: -135}:{y: 50, opacity: 0}}
+                            animate={theme === "dark" ? {rotate: 0}:{y: 0, opacity: 1}}
+                >
+                    {theme === "light" ?
+                        <MoonIcon className={"h-auto w-10"} stroke={`rgb(${variables.text})`}/> :
+                        <SunIcon className={"h-auto w-10"} stroke={`rgb(${variables.text})`}/>
+                    }
+                </motion.div>
             </label>
             <input id={"theme-switcher"}
                    className={"hidden"}
